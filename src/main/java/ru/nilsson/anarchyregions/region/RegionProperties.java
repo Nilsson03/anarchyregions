7b766c0b3394f3c2f903506e061a1d325f9035fb
Nilsson03 <nilsson_nicholas@mail.ru>
2025-10-11 02:22:36 +0300

    Enhance RegionProperties to include durability and radius attributes for improved region configuration management.

--- src/main/java/ru/nilsson/anarchyregions/region/RegionProperties.java
+++ src/main/java/ru/nilsson/anarchyregions/region/RegionProperties.java
@@ -12,11 +12,15 @@ public class RegionProperties {
     private final Namespace key;
     private final String displayName;
     private final Material blockType;
+    private final int durability;
+    private final int radius;
 
     public RegionProperties(ConfigurationSection section) {
         this.key = Namespace.of("anarchyregions",section.getString("key"));
         this.displayName = section.getString("display-name");
         this.blockType = Material.getMaterial(section.getString("block-type"));
+        this.durability = section.getInt("durability");
+        this.radius = section.getInt("radius");
     }
     
 }
