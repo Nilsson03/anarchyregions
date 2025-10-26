e69c0ae059d5cb0c5024387ea80f95e61372336b
Nilsson03 <nilsson_nicholas@mail.ru>
2025-10-26 22:16:57 +0300

    Изменено управление хранилищем регионов и их загрузка

--- src/main/java/ru/nilsson03/anarchyregions/storage/RegionStorage.java
+++ src/main/java/ru/nilsson03/anarchyregions/storage/RegionStorage.java
@@ -1,36 +1,31 @@
 package ru.nilsson03.anarchyregions.storage;
 
 import java.util.HashMap;
-import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
-import java.util.Optional;
-import java.util.Set;
 import java.util.UUID;
 
 import javax.annotation.Nullable;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 
 import lombok.Getter;
 import ru.nilsson03.anarchyregions.AnarchyRegions;
+import ru.nilsson03.anarchyregions.event.RegionsLoadEvent;
+import java.util.ArrayList;
+import ru.nilsson03.anarchyregions.properties.RegionProperties;
 import ru.nilsson03.anarchyregions.region.Region;
-import ru.nilsson03.anarchyregions.region.RegionProperties;
 import ru.nilsson03.library.bukkit.file.BukkitDirectory;
 import ru.nilsson03.library.bukkit.file.FileRepository;
 import ru.nilsson03.library.bukkit.file.configuration.BukkitConfig;
-import ru.nilsson03.library.bukkit.util.Namespace;
 import ru.nilsson03.library.bukkit.util.loc.LocationUtil;
 import ru.nilsson03.library.bukkit.util.log.ConsoleLogger;
 
 public class RegionStorage {
 
     private final BukkitDirectory regionsDirectory;
-    private final BukkitDirectory regionsDataDirectory;
-    @Getter
-    private final Set<RegionProperties> regionsProperties;
     @Getter
     private final Map<UUID, Region> regions;
 
@@ -38,24 +33,14 @@ public class RegionStorage {
         FileRepository fileRepository = plugin.fileRepository();
         
         try {
-            this.regionsDirectory = plugin.getDirectory("regions");
+            this.regionsDirectory = plugin.getDirectory("data");
             fileRepository.loadFiles(regionsDirectory, false);
-        } catch (IllegalStateException e) {
-            ConsoleLogger.error("anarchyregions", "Failed to load regions directory: %s", e.getMessage());
-            throw e;
-        }
-        
-        try {
-            this.regionsDataDirectory = plugin.getDirectory("data");
-            fileRepository.loadFiles(regionsDataDirectory, false);
         } catch (IllegalStateException e) {
             ConsoleLogger.error("anarchyregions", "Failed to load data directory: %s", e.getMessage());
             throw e;
         }
-        
-        ConsoleLogger.debug("anarchyregions", "Loading region storage...");
-        this.regionsProperties = loadRegionsProperties();
-        this.regions = loadRegions();
+
+        this.regions = new HashMap<>();
     }
 
     public void saveAllRegions() {
@@ -66,7 +51,7 @@ public class RegionStorage {
 
     public void saveRegion(Region region) {
         String regionIdString = region.getRegionId().toString();
-        BukkitConfig config = regionsDataDirectory.addNewConfig(regionIdString);
+        BukkitConfig config = regionsDirectory.addNewConfig(regionIdString);
         if (config != null) {
             FileConfiguration fileConfiguration = config.getFileConfiguration();
             fileConfiguration.set("center", LocationUtil.locationToString(region.getCenterLocation()));
@@ -83,65 +68,43 @@ public class RegionStorage {
         UUID regionId = region.getRegionId();
         regions.remove(regionId);
 
-        String regionIdString = regionId.toString();
+        String regionIdString = regionId.toString() + ".yml";
 
-        if (regionsDataDirectory.containsFileWithName(regionIdString)) {
-            BukkitConfig config = regionsDataDirectory.getBukkitConfig(regionIdString);
-            regionsDataDirectory.removeAndDeleteConfig(config);
+        if (regionsDirectory.containsFileWithName(regionIdString)) {
+            BukkitConfig config = regionsDirectory.getBukkitConfig(regionIdString);
+            regionsDirectory.removeAndDeleteConfig(config);
             ConsoleLogger.debug("anarchyregions", "Removed region %s from data directory", regionIdString);
         }
     }
 
-    public void createRegion(Location location, UUID regionOwner, RegionProperties properties) {
+    public Region createRegion(Location location, UUID regionOwner, RegionProperties properties) {
         Region region = new Region(location, regionOwner, properties);
         regions.put(region.getRegionId(), region);
         ConsoleLogger.debug("anarchyregions", "Created region %s in storage", region.getRegionId().toString());
+        return region;
     }
 
-    private Map<UUID, Region> loadRegions() {
-        Map<UUID, Region> regions = new HashMap<>();
+    public void loadRegions() {
         long startTime = System.currentTimeMillis();
-        for (BukkitConfig config : regionsDataDirectory.getCached()) {
+        List<Region> loadedRegions = new ArrayList<>();
+        
+        for (BukkitConfig config : regionsDirectory.getCached()) {
             UUID uuid = UUID.fromString(config.getName().replace(".yml", ""));
             Region region = new Region(uuid, config);
             regions.put(uuid, region);
+            loadedRegions.add(region);
         }
+        
+        if (!loadedRegions.isEmpty()) {
+            RegionsLoadEvent event = new RegionsLoadEvent(loadedRegions);
+            ConsoleLogger.info("anarchyregions", "Calling RegionsLoadEvent for %d regions", loadedRegions.size());
+            Bukkit.getPluginManager().callEvent(event);
+            ConsoleLogger.info("anarchyregions", "RegionsLoadEvent called and processed");
+        } else {
+            ConsoleLogger.info("anarchyregions", "No regions to load, skipping event");
+        }
+        
         ConsoleLogger.info("anarchyregions", "Loaded %d regions in %dms", regions.size(), System.currentTimeMillis() - startTime);
-        return regions;
-    }
-
-    private Set<RegionProperties> loadRegionsProperties() {
-        Set<RegionProperties> regions = new HashSet<>();
-        long startTime = System.currentTimeMillis();
-        for (BukkitConfig config : regionsDirectory.getCached()) {
-            ConfigurationSection section = config.getConfigurationSection("properties");
-            if (section == null) {
-                ConsoleLogger.warn("anarchyregions", "Region %s has no properties section", config.getName());
-                continue;
-            }
-
-            RegionProperties region = new RegionProperties(section);
-            regions.add(region);
-        }
-
-        ConsoleLogger.info("anarchyregions", "Loaded %d region properties in %dms", regions.size(), System.currentTimeMillis() - startTime);
-        return regions;
-    }
-
-    @Nullable
-    public RegionProperties getProperties(Namespace namespace) {
-        return regionsProperties.stream()
-                .filter(region -> region.getKey().equals(namespace))
-                .findFirst()
-                .orElse(null);
-    }
-
-    @Nullable
-    public RegionProperties getProperties(Material blockType) {
-        return regionsProperties.stream()
-                .filter(region -> region.getBlockType() == blockType)
-                .findFirst()
-                .orElse(null);
     }
 
     @Nullable
@@ -151,12 +114,17 @@ public class RegionStorage {
                 .findFirst()
                 .orElse(null);
     }
+    
+    @Nullable
+    public Region getRegion(UUID regionId) {
+        return regions.get(regionId);
+    }
 
     public void reload() {
         long startTime = System.currentTimeMillis();
+        regions.clear();
         regionsDirectory.reloadAll();
-        regionsProperties.clear();
-        regionsProperties.addAll(loadRegionsProperties());
+        loadRegions();
         ConsoleLogger.debug("anarchyregions", "Reloaded region storage in %dms", System.currentTimeMillis() - startTime);
     }
 }
