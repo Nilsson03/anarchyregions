88e38c6f6d7694142cf68492f810960280100c24
Nilsson03 <nilsson_nicholas@mail.ru>
2025-10-26 22:13:30 +0300

    Добавлена поддержка управления регионами через RegionManager и RegionUpdateService
    Интегрирован HologramManager для работы с конфигурацией, добавлены новые параметры для загрузки и сохранения регионов. Обновлена обработка событий через RegionListener с учетом новых зависимостей.

--- src/main/java/ru/nilsson03/anarchyregions/AnarchyRegions.java
+++ src/main/java/ru/nilsson03/anarchyregions/AnarchyRegions.java
@@ -1,19 +1,29 @@
 package ru.nilsson03.anarchyregions;
 
 import org.bukkit.Bukkit;
-import org.bukkit.plugin.java.JavaPlugin;
 
+import lombok.Getter;
+import ru.nilsson03.anarchyregions.hologram.HologramManager;
+import ru.nilsson03.anarchyregions.properties.PropertiesStorage;
 import ru.nilsson03.anarchyregions.region.listener.RegionListener;
+import ru.nilsson03.anarchyregions.region.manager.RegionManager;
+import ru.nilsson03.anarchyregions.service.RegionUpdateService;
 import ru.nilsson03.anarchyregions.storage.RegionStorage;
 import ru.nilsson03.library.NPlugin;
 import ru.nilsson03.library.bukkit.file.BukkitDirectory;
 import ru.nilsson03.library.bukkit.file.FileHelper;
 import ru.nilsson03.library.bukkit.file.configuration.BukkitConfig;
+import ru.nilsson03.library.bukkit.file.configuration.ParameterFile;
 
+@Getter
 public class AnarchyRegions extends NPlugin {
 
+    @Getter
     private static AnarchyRegions instance;
     private RegionStorage regionStorage;
+    private ParameterFile configFile;
+    private BukkitDirectory inventoriesDirectory;
+    private RegionUpdateService regionUpdateService;
 
     @Override
     public void enable() {
@@ -21,33 +31,56 @@ public class AnarchyRegions extends NPlugin {
 
         FileHelper.loadConfigurations(this,
             getDataFolder(),
+            "config.yml",
             "messages.yml"
+        );
+
+        FileHelper.loadConfigurations(this,
+            getDataFolder(),
+            "inventories/region-menu.yml"
         );
 
         FileHelper.loadConfiguration(this, "regions/example.yml");
 
         this.fileRepository().addExcludePaths(
             "data",
+            "config.yml",
             "regions"
         );
 
-        BukkitDirectory rooDirectory = getDirectory("/");
-        BukkitConfig messagesConfig = rooDirectory.getBukkitConfig("messages.yml");
+        configFile = ParameterFile.of(this, "config.yml");
 
-         regionStorage = new RegionStorage(this);
+        BukkitDirectory rootDirectory = getDirectory("/");
+        inventoriesDirectory = getDirectory("inventories");
+        
+        BukkitConfig messagesConfig = rootDirectory.getBukkitConfig("messages.yml");
 
-        RegionListener regionListener = new RegionListener(regionStorage, messagesConfig);
-        getServer().getPluginManager().registerEvents(regionListener, (JavaPlugin)this);
+        PropertiesStorage propertiesStorage = PropertiesStorage.getInstance(this);
 
+
+        new HologramManager(this, configFile);
+
+        regionStorage = new RegionStorage(this);
+
+        RegionManager regionManager = new RegionManager(regionStorage, configFile);
+        regionUpdateService = new RegionUpdateService(regionManager, configFile);
+        regionManager.setRegionUpdateService(regionUpdateService);
+        Bukkit.getPluginManager().registerEvents(regionManager, this);
+
+        regionStorage.loadRegions();
+        
+        RegionListener regionListener = new RegionListener(messagesConfig, propertiesStorage, regionManager);
+        Bukkit.getPluginManager().registerEvents(regionListener, this);
     }
 
     @Override
     public void disable() {
-        regionStorage.saveAllRegions();
+        if (regionUpdateService != null) {
+            regionUpdateService.shutdown();
+        }
+        if (regionStorage != null) {
+            regionStorage.saveAllRegions();
+        }
         Bukkit.getScheduler().cancelTasks(this); // Останавливаем оставшиеся таски плагина, работающие через Bukkit API
-    }
-
-    public static AnarchyRegions getInstance() {
-        return instance;
     }
 }
