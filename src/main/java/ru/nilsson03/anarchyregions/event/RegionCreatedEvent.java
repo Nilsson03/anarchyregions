7ee7da715cab02525b2db66aaeef3caa40b876bb
Nilsson03 <nilsson_nicholas@mail.ru>
2025-10-28 22:27:17 +0300

    Событие создания региона

---
+++ src/main/java/ru/nilsson03/anarchyregions/event/RegionCreatedEvent.java
@@ -0,0 +1,25 @@
+package ru.nilsson03.anarchyregions.event;
+
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import ru.nilsson03.anarchyregions.region.Region;
+
+@Getter
+@AllArgsConstructor
+public class RegionCreatedEvent extends Event {
+    
+    private static final HandlerList handlers = new HandlerList();
+    private final Region region;
+
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+    
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+}
