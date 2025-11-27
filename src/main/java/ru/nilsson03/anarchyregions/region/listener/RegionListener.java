2226c60db984d7fe2f8454714486871c415527ad
Nilsson03 <nilsson_nicholas@mail.ru>
2025-11-27 20:08:41 +0300

    Рефакторинг форматирования кода и замена метода formatLocation на location.toString()

--- src/main/java/ru/nilsson03/anarchyregions/region/listener/RegionListener.java
+++ src/main/java/ru/nilsson03/anarchyregions/region/listener/RegionListener.java
@@ -1,18 +1,16 @@
 package ru.nilsson03.anarchyregions.region.listener;
 
+import lombok.AllArgsConstructor;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.entity.Player;
-
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.block.Block;
-
-import lombok.AllArgsConstructor;
 import ru.nilsson03.anarchyregions.menu.RegionMenu;
 import ru.nilsson03.anarchyregions.properties.PropertiesStorage;
 import ru.nilsson03.anarchyregions.properties.RegionProperties;
@@ -40,30 +38,45 @@ public class RegionListener implements Listener {
 
         Region region = regionManager.getRegionByLocation(location);
 
-        if (!regionManager.isOwner(region, player.getUniqueId()) && !playerIsAdmin(player)) {
+        if (
+            !regionManager.isOwner(region, player.getUniqueId()) &&
+            !playerIsAdmin(player)
+        ) {
             event.setCancelled(true);
-            player.sendMessage(messagesConfig.getString("messages.region_not_owner"));
-            ConsoleLogger.warn("anarchyregions", "Player %s tried to break block in region at %s (not owner)",
-                    player.getName(), formatLocation(location));
+            player.sendMessage(
+                messagesConfig.getString("messages.region_not_owner")
+            );
+            ConsoleLogger.warn(
+                "anarchyregions",
+                "Player %s tried to break block in region at %s (not owner)",
+                player.getName(),
+                location.toString()
+            );
             return;
         }
 
         Location centerLocation = region.getCenterLocation();
 
-        // Проверяем по координатам (location.equals() не работает после разрушения
-        // блока)
-        boolean isCenterBlock = location.getWorld().equals(centerLocation.getWorld()) &&
-                location.getBlockX() == centerLocation.getBlockX() &&
-                location.getBlockY() == centerLocation.getBlockY() &&
-                location.getBlockZ() == centerLocation.getBlockZ();
+        // Проверяем по координатам (location.equals() не работает после разрушения блока)
+        boolean isCenterBlock =
+            location.getWorld().equals(centerLocation.getWorld()) &&
+            location.getBlockX() == centerLocation.getBlockX() &&
+            location.getBlockY() == centerLocation.getBlockY() &&
+            location.getBlockZ() == centerLocation.getBlockZ();
 
         if (isCenterBlock && block.getType() == region.getBlockType()) {
             boolean destroy = region.destroy();
 
             if (destroy) {
-                player.sendMessage(messagesConfig.getString("messages.region_destroyed"));
-                ConsoleLogger.info("anarchyregions", "Player %s destroyed region at %s",
-                        player.getName(), formatLocation(location));
+                player.sendMessage(
+                    messagesConfig.getString("messages.region_destroyed")
+                );
+                ConsoleLogger.info(
+                    "anarchyregions",
+                    "Player %s destroyed region at %s",
+                    player.getName(),
+                    location.toString()
+                );
             }
         }
     }
@@ -74,8 +87,11 @@ public class RegionListener implements Listener {
         Block clickedBlock = event.getClickedBlock();
 
         if (clickedBlock == null || clickedBlock.getType() == Material.AIR) {
-            ConsoleLogger.debug("anarchyregions",
-                    "Player %s tried to interact with region (clicked block is null or air)", player.getName());
+            ConsoleLogger.debug(
+                "anarchyregions",
+                "Player %s tried to interact with region (clicked block is null or air)",
+                player.getName()
+            );
             return;
         }
 
@@ -85,18 +101,36 @@ public class RegionListener implements Listener {
             Region region = regionManager.getRegionByLocation(location);
             Location coreLocation = region.getCenterLocation();
 
-            ConsoleLogger.debug("anarchyregions", "Location: %s, Core Location: %s", location.toString(), coreLocation.toString());
+            ConsoleLogger.debug(
+                "anarchyregions",
+                "Location: %s, Core Location: %s",
+                location.toString(),
+                coreLocation.toString()
+            );
 
-            if (location.equals(coreLocation) && event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+            if (
+                location.equals(coreLocation) &&
+                event.getAction() == Action.RIGHT_CLICK_BLOCK
+            ) {
                 if (regionManager.isOwner(region, player.getUniqueId())) {
-                    ConsoleLogger.debug("anarchyregions", "Player %s is owner of region at %s, opening menu",
-                            player.getName(), formatLocation(location));
+                    ConsoleLogger.debug(
+                        "anarchyregions",
+                        "Player %s is owner of region at %s, opening menu",
+                        player.getName(),
+                        location.toString()
+                    );
                     RegionMenu.openMenu(player, region);
                 } else {
-                    ConsoleLogger.debug("anarchyregions", "Player %s is not owner of region at %s, cancelling event",
-                            player.getName(), formatLocation(location));
+                    ConsoleLogger.debug(
+                        "anarchyregions",
+                        "Player %s is not owner of region at %s, cancelling event",
+                        player.getName(),
+                        location.toString()
+                    );
                     event.setCancelled(true);
-                    player.sendMessage(messagesConfig.getString("messages.region_not_owner"));
+                    player.sendMessage(
+                        messagesConfig.getString("messages.region_not_owner")
+                    );
                 }
             }
         }
@@ -114,9 +148,15 @@ public class RegionListener implements Listener {
         if (properties == null) {
             if (regionManager.regionExistsInLocation(location)) {
                 Region region = regionManager.getRegionByLocation(location);
-                if (region != null && !regionManager.isOwner(region, player.getUniqueId()) && !playerIsAdmin(player)) {
+                if (
+                    region != null &&
+                    !regionManager.isOwner(region, player.getUniqueId()) &&
+                    !playerIsAdmin(player)
+                ) {
                     event.setCancelled(true);
-                    player.sendMessage(messagesConfig.getString("messages.region_not_owner"));
+                    player.sendMessage(
+                        messagesConfig.getString("messages.region_not_owner")
+                    );
                 }
             }
             return;
@@ -125,44 +165,56 @@ public class RegionListener implements Listener {
         if (regionManager.regionExistsInLocation(location)) {
             Region existingRegion = regionManager.getRegionByLocation(location);
             if (existingRegion != null) {
-                if (!regionManager.isOwner(existingRegion, player.getUniqueId()) && !playerIsAdmin(player)) {
+                if (
+                    !regionManager.isOwner(
+                        existingRegion,
+                        player.getUniqueId()
+                    ) &&
+                    !playerIsAdmin(player)
+                ) {
                     event.setCancelled(true);
-                    player.sendMessage(messagesConfig.getString("messages.region_not_owner"));
+                    player.sendMessage(
+                        messagesConfig.getString("messages.region_not_owner")
+                    );
                     return;
                 }
 
                 event.setCancelled(true);
-                player.sendMessage(messagesConfig.getString("messages.region_already_exists"));
+                player.sendMessage(
+                    messagesConfig.getString("messages.region_already_exists")
+                );
                 return;
             }
         }
 
-        if (regionManager.wouldRegionOverlap(location, properties.getRadius())) {
+        if (
+            regionManager.wouldRegionOverlap(location, properties.getRadius())
+        ) {
             event.setCancelled(true);
-            player.sendMessage(messagesConfig.getString("messages.region_overlap"));
-            ConsoleLogger.warn("anarchyregions", "Player %s tried to create overlapping region at %s",
-                    player.getName(), formatLocation(location));
+            player.sendMessage(
+                messagesConfig.getString("messages.region_overlap")
+            );
+            ConsoleLogger.warn(
+                "anarchyregions",
+                "Player %s tried to create overlapping region at %s",
+                player.getName(),
+                location.toString()
+            );
             return;
         }
 
         regionManager.createRegion(location, player.getUniqueId(), properties);
         player.sendMessage(messagesConfig.getString("messages.region_created"));
-        ConsoleLogger.info("anarchyregions", "Player %s created region %s at %s",
-                player.getName(), properties.getDisplayName(), formatLocation(location));
+        ConsoleLogger.info(
+            "anarchyregions",
+            "Player %s created region %s at %s",
+            player.getName(),
+            properties.getDisplayName(),
+            location.toString()
+        );
     }
 
     private boolean playerIsAdmin(Player player) {
         return player.hasPermission("anarchyregions.admin");
-    }
-
-    /**
-     * Форматирует локацию для логирования
-     */
-    private String formatLocation(Location location) {
-        return String.format("%s (%d, %d, %d)",
-                location.getWorld().getName(),
-                location.getBlockX(),
-                location.getBlockY(),
-                location.getBlockZ());
     }
 }
