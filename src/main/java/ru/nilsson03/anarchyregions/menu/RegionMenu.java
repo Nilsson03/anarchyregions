e451903dc35c9d572811d6cf022a8fe5e8de0d18
Nilsson03 <nilsson_nicholas@mail.ru>
2025-11-27 20:08:03 +0300

    Добавлен индекс региона игрока и количество участников в меню региона

--- src/main/java/ru/nilsson03/anarchyregions/menu/RegionMenu.java
+++ src/main/java/ru/nilsson03/anarchyregions/menu/RegionMenu.java
@@ -1,15 +1,15 @@
 package ru.nilsson03.anarchyregions.menu;
 
 import java.util.List;
-
 import org.bukkit.entity.Player;
-
 import ru.nilsson03.anarchyregions.AnarchyRegions;
 import ru.nilsson03.anarchyregions.menu.api.CustomItem;
 import ru.nilsson03.anarchyregions.menu.util.MenuUtil;
 import ru.nilsson03.anarchyregions.properties.PropertiesStorage;
 import ru.nilsson03.anarchyregions.properties.RegionProperties;
 import ru.nilsson03.anarchyregions.region.Region;
+import ru.nilsson03.anarchyregions.region.manager.RegionManager;
+import ru.nilsson03.anarchyregions.region.service.RegionIndexService;
 import ru.nilsson03.library.bukkit.file.configuration.BukkitConfig;
 import ru.nilsson03.library.text.util.ReplaceData;
 import xyz.xenondevs.invui.gui.Gui;
@@ -20,40 +20,49 @@ import xyz.xenondevs.invui.window.Window;
 public class RegionMenu {
 
     private static final BukkitConfig regionMenuConfig;
-    private static final AnarchyRegions anarchyRegions;
-    
+    private static final AnarchyRegions plugin;
+    private static final RegionIndexService regionIndexService;
+
+    private static final String[] menuStructure;
 
     static {
-        anarchyRegions = AnarchyRegions.getInstance();
-        regionMenuConfig = anarchyRegions.getInventoriesDirectory().getBukkitConfig("region-menu.yml");
+        plugin = AnarchyRegions.getInstance();
+        regionMenuConfig = plugin
+                .getInventoriesDirectory()
+                .getBukkitConfig("region-menu.yml");
+        regionIndexService = plugin.getRegionIndexService();
+        menuStructure = regionMenuConfig.getList("region-menu.structure").toArray(new String[0]);
     }
 
     public static void openMenu(Player player, Region region) {
+        String[] structure = menuStructure;
+        Builder<Gui, Normal> builder = Gui.normal().setStructure(structure);
 
+        RegionProperties properties = PropertiesStorage.getPropertiesForRegion(
+                region);
 
-        String[] structure = regionMenuConfig.operations().getList("region-menu.structure", new ReplaceData[0]).toArray(new String[0]);
-        Builder<Gui, Normal> builder = Gui.normal()
-                .setStructure(structure);
-
-        RegionProperties properties = PropertiesStorage.getPropertiesForRegion(region);
+                
 
         List<CustomItem> customItems = MenuUtil.parseSection(
-            anarchyRegions, 
-            regionMenuConfig.getConfigurationSection("region-menu.items.custom"),
+                plugin,
+                regionMenuConfig.getConfigurationSection(
+                        "region-menu.items.custom"),
+                new ReplaceData("{numeric}", regionIndexService.getPlayerRegionIndex(player.getUniqueId(), region.getRegionId())),
                 new ReplaceData("{radius}", properties.getRadius()),
-                new ReplaceData("{displayName}", properties.getDisplayName())
-        );
+                new ReplaceData("{displayName}", properties.getDisplayName()),
+                new ReplaceData("{membersCount}", region.getMembersCount()));
 
         customItems.forEach(item -> {
             builder.addIngredient(item.getChar(), item);
         });
 
         Gui gui = builder.build();
-        Window.single().setViewer(player)
-                .setTitle(regionMenuConfig.operations().getString("region-menu.title"))
+        Window.single()
+                .setViewer(player)
+                .setTitle(
+                        regionMenuConfig.operations().getString("region-menu.title"))
                 .setGui(gui)
                 .build()
                 .open();
     }
-    
 }
